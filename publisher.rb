a2f68e2eb1e88d755bf5eec364fffb60c6a92bc3
boni <boni@twine.hu>
2014-10-16 17:36:47 +0200

    publisher sync amqp connection

--- publisher.rb
+++ publisher.rb
@@ -1,9 +1,31 @@
 $LOAD_PATH.unshift(File.expand_path('../lib', __FILE__))
+#$LOAD_PATH.unshift(File.expand_path('../lib/amqp', __FILE__))
 
+require 'json'
 require 'bundler/setup'
-Bundler.require(:default, ENV.fetch("RACK_ENV") { "default" })
+Bundler.require(:default, ENV.fetch("RACK_ENV") { "development" })
 
 
 class Publisher < Sinatra::Base
 
+  get '/' do
+    erb :form
+  end
+
+  post '/message' do
+    AMQP.start("amqp://127.0.0.1:5672") do |connection|
+      channel  = AMQP::Channel.new(connection)
+      queue    = channel.queue("amqpgem.examples.helloworld", :auto_delete => true)
+      exchange = channel.fanout("testing")
+      puts params.inspect
+      exchange.publish({ message: params[:message], lang: params[:lang], direction: params[:direction] }.to_json)
+      puts "Connected to AMQP broker. Running #{AMQP::VERSION} version of the gem..."
+      EventMachine.add_timer(2) do
+        exchange.delete
+
+        connection.close { EventMachine.stop }
+      end
+    end
+    redirect to('/')
+  end
 end
