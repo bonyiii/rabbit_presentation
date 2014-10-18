d78c0b5b586e449401848a13039886a2deca518b
bonyiii <boni@twine.hu>
2014-10-18 18:04:48 +0200

    Update rabbit_setup.sh
    update vhost and username

--- bin/rabbit_setup.sh
+++ bin/rabbit_setup.sh
@@ -1,3 +1,3 @@
-rabbitmqctl add_user boni password
-rabbitmqctl add_vhost demo
-rabbitmqctl set_permissions -p demo boni ".*" ".*" ".*"
+rabbitmqctl add_user demo_user password
+rabbitmqctl add_vhost demo_vhost
+rabbitmqctl set_permissions -p demo_vhost demo_user ".*" ".*" ".*"
