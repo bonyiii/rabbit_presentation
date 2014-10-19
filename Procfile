1b9a873f9fffd941a565d4b56e57ef6d5f397101
boni <boni@twine.hu>
2014-10-19 11:02:24 +0200

    foreman added

---
+++ Procfile
@@ -0,0 +1,2 @@
+publisher: bundle exec rackup publisher.rb -p 9292
+cli_consumer: bundle exec ruby cli_consumer.rb
--- publisher.rb
+++ publisher.rb
@@ -1,3 +1,10 @@
+$LOAD_PATH.unshift(File.expand_path('../lib', __FILE__))
+
+require 'json'
+require 'bundler/setup'
+Bundler.require(:default, ENV.fetch("RACK_ENV") { "development" })
+
+require File.expand_path("../publisher", __FILE__)
 require 'demo_amqp'
 
 #set :bind, '0.0.0.0'
